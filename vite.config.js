350904151b5b849cde71f4c3ae1d5082bee38816
ee ee <48785345-worldstarairwa1@users.noreply.replit.com>
2025-10-19 18:01:47 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
+  build: {
+    outDir: 'dist',
+  },
+  ...(command === 'serve'
+    ? {
+        server: {
+          host: '0.0.0.0',
+          port: 5000,
+          strictPort: true,
+          proxy: {
+            '/api': {
+              target: 'http://localhost:3001',
+              changeOrigin: true,
+            },
+          },
+        },
       }
-    }
-  }
-})
+    : {})
+}))
